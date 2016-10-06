e46406372060ddd445721b4055df8dc199a7a659
Zillah <eclypsis@mail.ru>
2016-10-06 23:02:13 +0300

    getMessage

---
+++ src/js/check.js
@@ -0,0 +1,31 @@
+'use strict';
+
+function getMessage(a, b) {
+  if (typeof a == 'boolean') {
+    if (a == true) {
+      return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
+    }
+    else {
+      return 'Переданное изображение на анимировано';
+    }
+
+  } else if (typeof a == 'number') {
+      return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' атрибутов';
+
+  } else if ((typeof a == 'object') && (typeof b == 'object')) {
+      var artifactsSquare = 0;
+      for (var i = 0, j = 0; i < a.length, j < b.length; i++, j++) {
+        artifactsSquare += a[i] * b[j];
+      }
+      return 'Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей';
+
+  } else if (typeof a == 'object') {
+      var amountOfRedPoints = 0;
+      for (var i = 0; i < a.length; i++) {
+      amountOfRedPoints += a[i];
+    }
+    return 'Количество красных точек во всех строчках изображения: ' + amountOfRedPoints;
+  }
+
+  return 'Переданы некорректные данные';
+}
