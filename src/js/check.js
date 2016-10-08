ae6e4558e87bdfc1db7b78a479e5d849d3fc39d2
Zillah <eclypsis@mail.ru>
2016-10-08 17:14:49 +0300

    getMeassage styleguide

--- src/js/check.js
+++ src/js/check.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function getMessage(a, b) {
-  if (typeof a == 'boolean') {
+  if (typeof a === 'boolean') {
     if (a == true) {
       return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
     }
@@ -9,20 +9,26 @@ function getMessage(a, b) {
       return 'Переданное изображение на анимировано';
     }
 
-  } else if (typeof a == 'number') {
+  } else if (typeof a === 'number') {
       return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' атрибутов';
 
-  } else if ((typeof a == 'object') && (typeof b == 'object')) {
-      var artifactsSquare = 0;
-      for (var i = 0, j = 0; i < a.length, j < b.length; i++, j++) {
-        artifactsSquare += a[i] * b[j];
-      }
-      return 'Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей';
+  } else if ((typeof a === 'object') && (typeof b == 'object')) {
+      if (Array.isArray(a) && Array.isArray(b) && a.length == b.length) {
+        var artifactsSquare = 0;
 
-  } else if (typeof a == 'object') {
-      var amountOfRedPoints = 0;
-      for (var i = 0; i < a.length; i++) {
-      amountOfRedPoints += a[i];
+        for (var i = 0, j = 0; i < a.length, j < b.length; i++, j++) {
+          artifactsSquare += a[i] * b[j];
+        }
+        return 'Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей';
+      }
+
+  } else if (typeof a === 'object') {
+      if (Array.isArray(a)) {
+        var amountOfRedPoints = 0;
+
+        for (var i = 0; i < a.length; i++) {
+          amountOfRedPoints += a[i];
+      }
     }
     return 'Количество красных точек во всех строчках изображения: ' + amountOfRedPoints;
   }
