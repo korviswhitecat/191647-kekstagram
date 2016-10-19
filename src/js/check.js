b91397c2fdee4df631f3366751aa0cdd26539eff
Zillah <eclypsis@mail.ru>
2016-10-19 22:32:39 +0300

    check fix

--- src/js/check.js
+++ src/js/check.js
@@ -2,33 +2,32 @@
 
 (function getMessage(a, b) {
   if (typeof a === 'boolean') {
-    if (a === true) {
+    if (a) {
       return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
-    } else {
-      return 'Переданное изображение на анимировано';
     }
+    return 'Переданное изображение на анимировано';
 
   } else if (typeof a === 'number') {
     return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' атрибутов';
 
   } else if ((typeof a === 'object') && (typeof b === 'object')) {
     if (Array.isArray(a) && Array.isArray(b) && a.length === b.length) {
-        var artifactsSquare = 0;
+      var artifactsSquare = 0;
 
-        for (var i = 0; i < a.length; i++) {
-          artifactsSquare += a[i] * a[i];
-        }
-        return 'Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей';
+      for (var i = 0; i < a.length; i++) {
+        artifactsSquare += a[i] * a[i];
       }
+      return 'Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей';
+    }
 
   } else if (typeof a === 'object') {
     if (Array.isArray(a)) {
-        var amountOfRedPoints = 0;
+      var amountOfRedPoints = 0;
 
-        for (var i = 0; i < a.length; i++) {
-          amountOfRedPoints += a[i];
-        }
+      for (var i = 0; i < a.length; i++) {
+        amountOfRedPoints += a[i];
       }
+    }
     return 'Количество красных точек во всех строчках изображения: ' + amountOfRedPoints;
   }
 
