5549d11297f3d18c02172246376e4b00024e0cc8
Zillah <eclypsis@mail.ru>
2016-10-27 18:32:32 +0300

    модули fix

--- src/js/pictures.js
+++ src/js/pictures.js
@@ -1,19 +1,22 @@
 'use strict';
 
-module.exports = function renderPictures(render) {
-
+module.exports = function() {
   var load = require('./load');
   var getPictureElement = require('./picture');
+
 
   var picturesContainer = document.querySelector('.pictures');
   var filters = document.querySelector('.filters');
   var IMAGE_LOAD_URL = 'http://localhost:1507/api/pictures';
 
-  filters.classList.add('hidden');
-  render.forEach(function(picture) {
-    picturesContainer.appendChild(getPictureElement(picture));
-  });
+  var renderPictures = function(render) {
+    filters.classList.add('hidden');
+    render.forEach(function(picture) {
+      picturesContainer.appendChild(getPictureElement(picture));
+    });
 
-  filters.classList.remove('hidden');
+    filters.classList.remove('hidden');
+  };
+
   load(IMAGE_LOAD_URL, renderPictures);
 };
