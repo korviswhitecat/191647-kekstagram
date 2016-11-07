63ae94c59fcd0aa9a44930d25777970c07e1a4e7
Zillah <eclypsis@mail.ru>
2016-11-07 17:28:36 +0300

    второй класс

--- src/js/picture.js
+++ src/js/picture.js
@@ -2,8 +2,56 @@
 
 var gallery = require('./gallery');
 
-var getPictureElement = function(picture) {
+// var getPictureElement = function(picture) {
+//
+//   var template = document.querySelector('template');
+//   var templateContainer = 'content' in template ? template.content : template;
+//   var pictureElement = templateContainer.querySelector('.picture').cloneNode(true);
+//   var img = pictureElement.querySelector('img');
+//   var elementImage = new Image();
+//   var imageLoadTimeout = null;
+//   var IMAGE_LOAD_TIMEOUT = 10000;
+//
+//   pictureElement.querySelector('.picture-comments').textContent = picture.comments;
+//   pictureElement.querySelector('.picture-likes').textContent = picture.likes;
+//   elementImage.src = picture.url;
+//   imageLoadTimeout = setTimeout(function() {
+//     elementImage.classList.add('picture-load-failure');
+//     elementImage.width = 182;
+//     elementImage.height = 182;
+//   }, IMAGE_LOAD_TIMEOUT);
+//
+//   elementImage.onload = function(evt) {
+//     clearTimeout(imageLoadTimeout);
+//     img.src = evt.target.src;
+//     elementImage.width = 182;
+//     elementImage.height = 182;
+//   };
+//
+//   elementImage.onerror = function() {
+//     pictureElement.classList.add('picture-load-failure');
+//     elementImage.width = 182;
+//     elementImage.height = 182;
+//   };
+//
+//   return pictureElement;
+// };
 
+var Picture = function(picture, index) {
+  this.data = picture;
+  this.element = getPictureElement(picture, index);
+  var self = this;
+  this.element.onclick = function(evt) {
+    evt.preventDefault();
+    if(!this.classList.contains('picture-load-failure')) {
+      gallery.show(index);
+    }
+  };
+  this.remove = function() {
+    self.element.onclick = null;
+  };
+};
+Picture.prototype = { getPictureElement: function(picture) {
   var template = document.querySelector('template');
   var templateContainer = 'content' in template ? template.content : template;
   var pictureElement = templateContainer.querySelector('.picture').cloneNode(true);
@@ -34,30 +82,7 @@ var getPictureElement = function(picture) {
     elementImage.height = 182;
   };
 
-  // pictureElement.onclick = function(evt) {
-  //   evt.preventDefault();
-  //
-  //   if(!this.classList.contains('picture-load-failure')) {
-  //     gallery.show(index);
-  //   }
-  // };
-
   return pictureElement;
-};
-
-var Picture = function(picture, index) {
-  this.data = picture;
-  this.element = getPictureElement(picture, index);
-  var self = this;
-  this.element.onclick = function(evt) {
-    evt.preventDefault();
-    if(!this.classList.contains('picture-load-failure')) {
-      gallery.show(index);
-    }
-  };
-  this.remove = function() {
-    self.element.onclick = null;
-  };
-};
+} };
 
 module.exports = Picture;
