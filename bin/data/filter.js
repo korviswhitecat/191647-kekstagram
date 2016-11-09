c0bd3596786b2a690194a26a70779bda11371ef3
Zillah <eclypsis@mail.ru>
2016-11-09 21:50:06 +0300

    фиксы по постраничной отрисовке

--- bin/data/filter.js
+++ bin/data/filter.js
@@ -6,6 +6,7 @@ module.exports = function(list, filterID) {
           return list.sort(function(a, b) {
       return b.likes - a.likes;
     });
+    break;
 
     case 'filter-new' :
       list.sort(function(a, b) {
@@ -14,11 +15,14 @@ module.exports = function(list, filterID) {
       return list = list.filter(function(date) {
         return date.created > list[0].created - 1000 * 60 * 60 * 24 * 3;
       });
+      break;
+
 
     case 'filter-discussed' :
       return list.sort(function(a, b) {
         return b.comments - a.comments;
       });
+      break;
     }
 
   return list;
--- src/js/load.js
+++ src/js/load.js
@@ -1,21 +1,19 @@
 'use strict';
 
-var load = function(IMAGE_LOAD_URL, callback) {
+var load = function(url, params, callback) {
+
+  var getSearchString = function() {
+    return Object.keys(params).map(function(param) {
+      return [param, params[param]].join('=');
+    }).join('&');
+  };
+
   var xhr = new XMLHttpRequest();
-
-  xhr.open('GET', IMAGE_LOAD_URL);
-
-  // xhr.open('GET', IMAGE_LOAD_URL + '?filter=' + params.filter + '&from=' + params.from + '&to=' + params.to);
-
-  xhr.addEventListener('load', function(evt) {
-    try {
-      var loadedData = JSON.parse(evt.target.response);
-      callback(loadedData);
-    } catch(error) {
-      console.log(error);
-    }
-  });
-
+  xhr.onload = function(evt) {
+    var loadedData = JSON.parse(evt.target.response);
+    callback(loadedData);
+  };
+  xhr.open('GET', url + '?' + getSearchString(params));
   xhr.send();
 };
 
