e7e60c573a2b6a87a807f73a0eafea02051109f6
Zillah <eclypsis@mail.ru>
2016-11-10 20:50:15 +0300

    отрисовка

--- src/js/pictures.js
+++ src/js/pictures.js
@@ -7,7 +7,7 @@ var gallery = require('./gallery');
 module.exports = function() {
   var picturesContainer = document.querySelector('.pictures');
   var filters = document.querySelector('.filters');
-  // var footer = document.querySelector('.footer');
+  var pictures = [];
 
   var THROTTLE_TIMEOUT = 100;
   var PAGE_SIZE = 12;
@@ -23,19 +23,11 @@ module.exports = function() {
 
   var renderPictures = function(render) {
     gallery.setPictures(render);
-
     picturesContainer.innerHTML = '';
-
     filters.classList.add('hidden');
     render.forEach(function(picture, index) {
       picturesContainer.appendChild(new Picture(picture, index).element);
     });
-
-    // if (picturesContainer.getBoundingClientRect().height - 120 < window.innerHeight - footer.getBoundingClientRect().height) {
-    //   pageNumber++;
-    //   setFilterProperties();
-    //   // reLoad();
-    // }
 
     filters.classList.remove('hidden');
   };
@@ -59,8 +51,10 @@ module.exports = function() {
 
   var reLoad = function() {
     load(IMAGE_LOAD_URL, params, function(data) {
-      renderPictures(data);
-      if (isBottomReached() && isNextPageAvailable(data, pageNumber, PAGE_SIZE)) {
+      pictures = data;
+
+      renderPictures(pictures);
+      if (isBottomReached() && isNextPageAvailable(pictures, pageNumber, PAGE_SIZE)) {
         pageNumber++;
         setFilterProperties();
         reLoad();
@@ -85,13 +79,12 @@ module.exports = function() {
 
     window.addEventListener('scroll', function() {
       if (Date.now() - lastCall >= THROTTLE_TIMEOUT) {
-        if (isBottomReached() && isNextPageAvailable(data, pageNumber, PAGE_SIZE)) {
-          load(IMAGE_LOAD_URL, params, renderPictures);
+        if (isBottomReached() && isNextPageAvailable(pictures, pageNumber, PAGE_SIZE)) {
           pageNumber++;
           setFilterProperties();
-          lastCall = Date.now();
-
+          load(IMAGE_LOAD_URL, params, renderPictures);
         }
+        lastCall = Date.now();
       }
     });
   };
